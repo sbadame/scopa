4fdc65bf0ddef4d24b1d843177c5e1e76c675990
sandro <sbadame@gmail.com>
2020-04-02 00:52:46 -0400

    Add a mechanism for the current active player.

--- server.go
+++ server.go
@@ -8,18 +8,23 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
+	"sync"
 )
 
 // To test with no random seed:
-// $ curl -v  localhost:8080/state
-// $ curl -v  localhost:8080/take -H "Content-Type: application/json" --data '{"Card": {"Suite":4, "Value":2}, "Table": [{"Suite": 1, "Value": 2}]}'
-// $ curl -v  localhost:8080/drop -H "Content-Type: application/json" --data '{"Card": {"Suite":4, "Value":2}}'
+// $ curl -v localhost:8080/join
+// $ curl -v localhost:8080/state
+// $ curl -v localhost:8080/take -H "Content-Type: application/json" --data '{"PlayerId": 1, "Card": {"Suite":4, "Value":2}, "Table": [{"Suite": 1, "Value": 2}]}'
+// $ curl -v localhost:8080/drop -H "Content-Type: application/json" --data '{"Card": {"Suite":4, "Value":2}}'
 
 type drop struct {
+	PlayerId int
 	Card scopa.Card
 }
 
 type take struct {
+	PlayerId int
 	Card  scopa.Card
 	Table []scopa.Card
 }
@@ -49,6 +54,27 @@ func parseRequestJson(w http.ResponseWriter, r *http.Request, v interface{}) boo
 
 func main() {
 	state := scopa.NewGame()
+	gameId := time.Now().Unix()
+
+	// Keep track of the number of players that have "joined".
+	// Give them a player id.
+	var playerCount int
+	var playerMux sync.Mutex
+
+	// Serve resources for testing.
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+
+	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
+		playerMux.Lock()
+		defer playerMux.Unlock()
+		if playerCount >= 2 {
+			w.WriteHeader(400)
+			io.WriteString(w, fmt.Sprintf("{\"Message\": \"Game is full!\"}"))
+			return
+		}
+		io.WriteString(w, fmt.Sprintf("{\"PlayerId\": %d, \"GameId\": %d}", playerCount + 1, gameId))
+		playerCount += 1
+	})
 
 	http.HandleFunc("/state", func(w http.ResponseWriter, r *http.Request) {
 		if j, err := json.Marshal(state); err != nil {
@@ -65,6 +91,12 @@ func main() {
 			return
 		}
 
+		if d.PlayerId != state.NextPlayer {
+			w.WriteHeader(400)
+			io.WriteString(w, "{\"Message\": \"Not your turn!\"}")
+			return
+		}
+
 		if err := state.Drop(d.Card); err != nil {
 			w.WriteHeader(500)
 			io.WriteString(w, fmt.Sprintf("Couldn't drop: %v", err))
@@ -74,6 +106,12 @@ func main() {
 	http.HandleFunc("/take", func(w http.ResponseWriter, r *http.Request) {
 		var t take
 		if !parseRequestJson(w, r, &t) {
+			return
+		}
+
+		if t.PlayerId != state.NextPlayer {
+			w.WriteHeader(400)
+			io.WriteString(w, "{\"Message\": \"Not your turn!\"}")
 			return
 		}
 
