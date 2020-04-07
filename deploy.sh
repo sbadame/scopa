4bf402f720a7fbcc0ac914cbc183561d30e0d9f3
Sandro <sbadame@gmail.com>
2020-04-06 23:36:39 -0400

    Make deploy start the instance and remove the previous server.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-gcloud beta compute ssh --zone "us-east1-b" "scopaserver" --project "scopa-273021" --command 'mkdir -p scopa'
+go build server.go || exit 2
+
+gcloud compute instances start --zone "us-east1-b" "scopaserver" --project "scopa-273021" 
+gcloud beta compute ssh --zone "us-east1-b" "scopaserver" --project "scopa-273021" \
+  --command 'mkdir -p scopa; rm scopa/server'
 gcloud compute scp --zone "us-east1-b" --project "scopa-273021" --recurse server web 'scopaserver:~/scopa/'
