ff15ebbf8fff1020d10793cfac8f71f0ad802517
Sandro <sbadame@gmail.com>
2020-04-07 21:46:49 -0400

    add a package for automatically restarting the server when a new version is deployed.

---
+++ autoreload/autoreload.go
@@ -0,0 +1,31 @@
+package autoreload
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func restartOnUpdate() {
+	s, _ := os.Readlink("/proc/self/exe")
+	fmt.Printf("This program will restart itself with the same flags and environment variables when %s changes.\n", s)
+
+	for {
+		time.Sleep(1 * time.Second)
+		s, _ := os.Readlink("/proc/self/exe")
+		if strings.HasSuffix(s, " (deleted)") {
+			p := s[:len(s)-10]
+			fmt.Printf("Restarting %s\n", s)
+			if err := syscall.Exec(p, os.Args, os.Environ()); err != nil {
+				fmt.Printf("Autoreload failed! %s: %v", p, err)
+			}
+		}
+
+	}
+}
+
+func init() {
+	go restartOnUpdate()
+}
