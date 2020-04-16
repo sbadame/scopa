73a1951f2d211584e0a16ddffc5d62968b952b5b
sandro <sbadame@gmail.com>
2020-04-16 00:07:58 -0400

    Add the gce startup script.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,20 @@
 
 go build cmd/server.go || exit 2
 
-gcloud compute instances start --zone "us-east1-b" "scopaserver" --project "scopa-273021" 
+gcloud compute instances add-metadata --zone "us-east1-b" "scopaserver" --project "scopa-273021" --metadata startup-script='
+#!/bin/bash
+
+# Make sure that we have setcap
+sudo apt-get install libcap2-bin
+
+cd /home/sandro/scopa
+sudo setcap cap_net_bind_service=+ep server
+
+# Run the server with dropped permissions.
+sudo -u sandro /home/sandro/scopa/server -https_host scopa.sandr.io -random
+'
+
+gcloud compute instances start --zone "us-east1-b" "scopaserver" --project "scopa-273021"
 gcloud beta compute ssh --zone "us-east1-b" "scopaserver" --project "scopa-273021" \
   --command 'mkdir -p scopa; rm scopa/server'
 gcloud compute scp --zone "us-east1-b" --project "scopa-273021" --recurse server web 'scopaserver:~/scopa/'
