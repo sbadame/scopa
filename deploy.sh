fce6679df54af58c088c9e2f98c064ec970158b5
sandro <sbadame@gmail.com>
2020-04-15 23:29:00 -0400

    Update the deploy script for the new location of the server.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go build cmd/server/server.go || exit 2
+go build cmd/server.go || exit 2
 
 gcloud compute instances start --zone "us-east1-b" "scopaserver" --project "scopa-273021" 
 gcloud beta compute ssh --zone "us-east1-b" "scopaserver" --project "scopa-273021" \
