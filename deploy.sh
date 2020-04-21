1262f5d01e081a593d33350d82a1584de022ea93
sandro <sbadame@gmail.com>
2020-04-21 19:42:47 -0400

    Add a message about which version was deployed.

--- deploy.sh
+++ deploy.sh
@@ -23,3 +23,5 @@ gcloud compute instances start --zone "us-east1-b" "scopaserver" --project "scop
 gcloud beta compute ssh --zone "us-east1-b" "scopaserver" --project "scopa-273021" \
   --command 'mkdir -p scopa; rm scopa/server'
 gcloud compute scp --zone "us-east1-b" --project "scopa-273021" --recurse server web 'scopaserver:~/scopa/'
+
+echo "Deployed $(git rev-parse HEAD)"
